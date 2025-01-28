528e05e3708eb31d7c1cb83a88ff6df72be88643
Forrest <forrestpatwalker@hotmail.com>
2025-01-28 12:33:24 -0700

    Update NoodleBot.py

--- NoodleBot.py
+++ NoodleBot.py
@@ -25,36 +25,36 @@ def bot_responses(text_file):
 
 # Function that will search posts on a given subreddit for keywords within the title and respond with a catchphrase
 def post_search(subreddit_name):
-    subreddit = reddit.subreddit(subreddit_name)  # store the subreddit to a variable
+    subreddit = reddit.subreddit(subreddit_name)
 
-    for submission in subreddit.new(limit=10):  # Loop through the 10 newest posts in given subreddit
-        for j in KEYWORDS_IN_TITLE:  # loop through keywords to search for posts
+    for submission in subreddit.new(limit=10):
+        for j in KEYWORDS_IN_TITLE:
             if submission.id in POSTS_ALREADY_RESPONDED_TO:
                 break
             if re.search(j, submission.title, re.IGNORECASE):
-                submission.reply(bot_responses("catchphrases.txt"))  # reply to the post with a catchphrase
-                print("Noodle Bot replying to:", submission.title, ", in subreddit", subreddit)  # print to console
-                POSTS_ALREADY_RESPONDED_TO.append(submission.id)  # add post id to list
+                submission.reply(bot_responses("catchphrases.txt"))
+                print("Noodle Bot replying to:", submission.title, ", in subreddit", subreddit)
+                POSTS_ALREADY_RESPONDED_TO.append(submission.id)
                 break
 
 
 # function that checks inbox and responds to new comment replies and responds based on if the given author has
 # replied to NoodleBot before.
 def comment_reply():
-    for mail in reddit.inbox.unread():  # loop through unread mail
-        if isinstance(mail, Comment):  # if unread mail is a comment
-            if str(mail.author) not in AUTHORS_RESPONDED_TO:  # if author to comment reply isn't stored to text file
-                mail.reply(  # response to their comment reply
+    for mail in reddit.inbox.unread():
+        if isinstance(mail, Comment):
+            if str(mail.author) not in AUTHORS_RESPONDED_TO:
+                mail.reply(
                     "Dear friend I am just a bot and not a smart one either so I have no words for your message. "
                     "If you truly must speak to me try messaging /u/foorast he's my creator and the reason I crave "
                     "noodles so much.")
-                print("I have responded to:", mail.author)  # print to console what was done
-                AUTHORS_RESPONDED_TO.append(str(mail.author))  # add authors name to the array
-                mail.mark_read()  # mark the message as read
+                print("I have responded to:", mail.author)
+                AUTHORS_RESPONDED_TO.append(str(mail.author))
+                mail.mark_read()
             else:
-                mail.reply(bot_responses("multiple_replies.txt"))  # reply with a random response from array
-                print("I have responded again to:", mail.author)  # print what happened to console
-                mail.mark_read()  # mark message as read
+                mail.reply(bot_responses("multiple_replies.txt"))
+                print("I have responded again to:", mail.author)
+                mail.mark_read()
 
 
 if __name__ == "__main__":
